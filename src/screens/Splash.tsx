6267fab52561c4aa7b7d3e5e3f3c6ee0d70ff10c
Jesus Rodriguez <Jesus.Rodriguez@techconsulting.tech>
2025-09-18 15:41:21 -0400

    Fonts Added

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,23 +1,29 @@
-import { Text, Button } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../utils/types/NavigationTypes';
+import { useEffect } from 'react';
 
 const Splash = () => {
   const navigation: NavigationProp<RootStackParamList> = useNavigation();
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   navigation.navigate('Feed');
+    // }, 5000);
+  });
   return (
     <SafeAreaView>
-      <Text>Splash</Text>
-      <Button
-        title="Navigate"
-        onPress={() => {
-          navigation.navigate('Feed');
-        }}
-      />
+      <Text style={styles.textLogo}>Jesus.It.Dev</Text>
     </SafeAreaView>
   );
 };
 
 export default Splash;
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textLogo: {
+    fontSize: 50,
+    fontFamily: 'Azeret Mono',
+  },
+});
